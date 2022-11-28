5a3cffd3c80fbbe070baed09fb51cfab85a0b90f
John <johnfarris217@gmail.com>
2022-11-27 19:42:16 -0600

    commit tree 9e2c36c05751a53455961410ba28fb5f7636dc6d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const newCards = [];
+    for (let i = 0; i < names.length; i++) {
+        newCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newCards;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
